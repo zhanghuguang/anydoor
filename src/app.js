2a71260a39b5297d1d530cef8232afee62cb9fec
章沪光 <zhanghuguang@linctex.com>
2019-11-21 14:29:45 +0800

    feat: 新增 自动打开网页

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 const path = require('path');
 const conf = require('./config/defaultConfig');
 const route = require('./helper/route');
-
+const openUrl = require('./helper/openUrl');
 class Server {
   constructor(config) {
     this.conf = Object.assign({}, conf, config);
@@ -18,6 +18,7 @@ class Server {
     server.listen(this.conf.port, this.conf.hostname, () => {
       const addr = `http://${this.conf.hostname}:${this.conf.port}`;
       console.info(`Server started at ${chalk.green(addr)}`);
+      openUrl(addr);
     });
   }
 }
